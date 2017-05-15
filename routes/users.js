cdd7d1e47863447b930a9730bf485082b749f930
Ahya <ayakurdi@gmail.com>
2017-05-15 14:04:22 -0700

    User Model set up with views and routes and authentication

---
+++ routes/users.js
@@ -0,0 +1,38 @@
+//user routes
+const
+  express = require('express'),
+  passport = require('passport'),
+  userRouter = express.Router()
+
+userRouter.route('/login')
+  .get((req,res) =>{res.render('users/login', {message:req.flash('loginMessage')})
+})
+  .post(passport.authenticate('local-login', {
+    successRedirect: '/profile',
+    failureRedirect: '/login'
+}))
+
+userRouter.route('/signup')
+  .get((req,res) =>{res.render('users/signup', {message: req.flash('signupMessage')})
+})
+  .post(passport.authenticate('local-signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup'
+  }))
+
+userRouter.get('/profile', isLoggedIn, (req,res) =>{
+  res.render('users/profile', {user:req.user})
+})
+
+userRouter.get('/logout', isLoggedIn, (req,res) =>{
+  req.logout()
+  res.redirect('/')
+})
+
+function isLoggedIn(req, res, next) {
+  if(req.isAuthenticated()) return next()
+  res.redirect('/')
+}
+
+
+module.exports = userRouter
