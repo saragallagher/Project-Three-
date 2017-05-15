43ccb8d308ced22dcb433868f58be58797c35692
Ahya <ayakurdi@gmail.com>
2017-05-15 11:11:02 -0700

    First Commit. Downloaded packages and set up folders

---
+++ server.js
@@ -0,0 +1,22 @@
+const
+  express = require('express'),
+  app = express(),
+  mongoose = require('mongoose'),
+  logger = require('morgan'),
+  bodyParser = require('body-parser'),
+  ejs = require('ejs'),
+	ejsLayouts = require('express-ejs-layouts'),
+  flash = require('connect-flash'),
+  cookieParser = require('cookie-parser'),
+  session = require('express-session'),
+  MongoDBStore = require('connect-mongodb-session')(session),
+  passport = require('passport'),
+  PORT= 3000
+
+app.get('/', (req, res) => {
+  res.json('Welcome to Travel Tasker')
+})
+
+app.listen(PORT, (err) => {
+  console.log(err || `Server listening on port ${PORT}. 🤘`)
+})
