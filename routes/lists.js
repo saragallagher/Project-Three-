1e9abd6ff9b45f1ed5f958838c5ce93a59583b62
Sara Gallagher <sara.p.gallagher@gmail.com>
2017-05-15 19:27:37 -0700

    got post, patch, and delete to work for task items~need to get post/patch to load automatically

--- routes/lists.js
+++ routes/lists.js
@@ -39,7 +39,6 @@ listRouter.route('/:id')
   })
 
 
-
 listRouter.route('/:id/tasks')
   .get(function(req, res){
     List.findById(req.params.id, (err, list) => {
@@ -48,13 +47,41 @@ listRouter.route('/:id/tasks')
     })
   })
   .post(function(req, res){
-    List.findById(req.params.id, (err, list) => {
+    //semi works need to refresh page in order for the list to update??
+    List.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, list) => {
       if(err) return console.log(err)
-      list.task.push(req.body)
-      list.save((err)=> {
-        res.json(list)
-      })
+      console.log(req.body);
+      var newTask = {}
+      newTask.body = req.body.body
+      newTask.completed = false
+      list.task.push(newTask)
+      list.save()
+      res.json(list)
+      // })
     })
   })
+
+listRouter.route('/:id/tasks/:taskId')
+  .patch(function(req, res){
+    //works but need to refresh to see changes
+    List.findByIdAndUpdate(req.params.id, req.body,{new: true}, function(err, list){
+      var newTask = list.task.id(req.params.taskId)
+      var index = list.task.indexOf(newTask)
+      list.task[index]= req.body
+      list.save()
+      res.json({message: 'updated list', task: newTask})
+    })
+  })
+  .delete(function(req, res){
+    List.findById(req.params.id, function(err, list){
+      var task = list.task.id(req.params.taskId)
+      var index = list.task.indexOf(task)
+      list.task.splice(index, 1)
+      list.save()
+      // task.remove()
+      res.json({message: 'Deleted'})
+    })
+  })
+
 
 module.exports = listRouter
